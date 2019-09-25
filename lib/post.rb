f0218142875d10ac331b51133da730426a269ae5
Kierra M <k.merriman32@hotmail.com>
2019-09-25 15:01:20 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  attr_accessor :author, :name
+ 
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
